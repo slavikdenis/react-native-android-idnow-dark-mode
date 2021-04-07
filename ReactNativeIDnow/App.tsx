231830978f06e1a8bea38a10db861718cf550dc5
Denis Slavik <denis.slavik@gmail.com>
2021-04-07 16:26:05 +0200

    Add example app

--- ReactNativeIDnow/App.tsx
+++ ReactNativeIDnow/App.tsx
@@ -8,108 +8,62 @@
  * @format
  */
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+  NativeModules,
+  Platform,
+  Button,
+} from 'react-native';
+const {IDnowModule} = NativeModules;
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+if (Platform.OS !== 'android') {
+  throw new Error('Only Android example!');
+}
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+// Fill accordingly
+const IDNOW_COMPANY_ID = '';
+const IDNOW_TRANSACTION_TOKEN = '';
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const Colors = {
+    white: '#FFFFFF',
+  };
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+  const backgroundStyle = {
+    flex: 1,
+    padding: 10,
+    backgroundColor: Colors.white,
+  };
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+  const startVideoIdent = async () => {
+    try {
+      await IDnowModule.startVideoIdent({
+        companyId: IDNOW_COMPANY_ID,
+        transactionToken: IDNOW_TRANSACTION_TOKEN,
+        showVideoOverviewCheck: true,
+        userInterfaceLanguage: 'en',
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle="dark-content" />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View style={{backgroundColor: Colors.white}}>
+          <Button onPress={startVideoIdent} title="Start VideoIdent" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
- export default App;
+export default App;
